d70f4cec4c9d7beef7648e53c1a7b7ab5b80f7cb
datafluisteraar <steven.gort@ictu.nl>
2022-06-29 08:48:09 +0200

    initial local ReSpec test

--- js/config.js
+++ js/config.js
@@ -3,42 +3,62 @@ var respecConfig =
   specStatus: "WV",
   specType: "HR",
   pubDomain: "dk",
-  shortName: "template",
+  subtitle: "Een werkmethode voor het doen van onderzoek door derden naar het gebruik van algoritmen door een overheidsorganisatie.",
+  shortName: "De LegitiMaat",
   publishDate: "2022-06-24",
   publishVersion: "0.0.1",
-  // previousPublishVersion: "(none)",
-  //  previousPublishDate: "(none)",
+  previousPublishVersion: "",
+  previousPublishDate: "",
   //  previousMaturity: "WV",
-  editors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
-  authors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
-  github: "https://github.com/Logius-standaarden/API-Design-Rules",
-  
-  
- // Controls if linked "§" section markers are added to a document
- addSectionLinks: true,
- 
-    // this parameter will add the tag_name of the latest release to the document Title
+  editors: [{
+    name: "Mildo Staden",
+    mailto: "Mildo.Staden@minbzk.nl",
+    company: "BZK",
+    companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+  }, {
+    name: "Marlies van Eck",
+    mailto: "marlies@hooghiemstra-en-partners.nl",
+    company: "Hooghiemstra & Partners",
+    companyURL: "https://hooghiemstra-en-partners.nl",
+  }],
+  authors: [{
+    name: "Marlies van Eck",
+    url: "https://hooghiemstra-en-partners.nl",
+  }, {
+    name: "Mariette Lokin",
+    url: "https://www.rijksoverheid.nl/ministeries/ministerie-van-financien",
+  }, {
+    name: "Maike Klip",
+    url: "https://www.nationaleombudsman.nl/",
+  }, {
+    name: "Giulia Bössenecker",
+    url: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+  }, {
+    name: "Carlijn Oldeman",
+    url: "https://hooghiemstra-en-partners.nl",
+  }, {
+    name: "Robert van Doesburg",
+    url: "https://www.tno.nl",
+  }, {
+    name: "Abram Klop",
+    url: "https://www.ubrijk.nl/service/i-interim-rijk",
+  }, {
+    name: "Steven Gort",
+    url: "https://www.ictu.nl",
+  }],
+  github: "https://github.com/sgort/LegitiMaat",
+
+  // Controls if linked "§" section markers are added to a document
+  addSectionLinks: true,
+
+  // this parameter will add the tag_name of the latest release to the document Title
   // only set this parameter when a release has been set
   nl_addReleaseTagTitle: true,
 
-    // nl_organisationName is used for some company specific values in the header (and Sotd)
+  // nl_organisationName is used for some company specific values in the header (and Sotd)
   // currently supported: Logius and Geonovum (default)  
-  nl_organisationName: "Logius",
-  
+  nl_organisationName: "MinBZK",
+
   // this url points to the folder where organsation specific css files are stored
   // defaults to https://tools.geostandaarden.nl/respec/style/ if not set
   nl_organisationStylesURL: "https://publicatie.centrumvoorstandaarden.nl/respec/style/",
@@ -58,14 +78,15 @@ var respecConfig =
   // nl_logo refers to company logo
   // defaults to https://tools.geostandaarden.nl/respec/style/logos/Geonovum.svg
   nl_logo: {
-    src: "https://publicatie.centrumvoorstandaarden.nl/respec/style/logos/figure-logius.svg",
-    alt: "Logius",
-    id: "Logius",
-    height: 77,
-    width: 44,
-    url: "https://www.logius.nl/standaarden",
+    src: "https://open-regels.nl/assets/images/delegitimaat.png",
+    alt: "LegitiMaat-logo",
+    id: "MinBZK",
+    height: 150,
+    width: 150,
+    url: "https://open-regels.nl",
   },
-
+  // enable automatic external reference linking
+  xref: true,
 
   localBiblio: {
     "SemVer": {
